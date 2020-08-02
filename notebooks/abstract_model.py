7c7ebe2183a0a7d75e157d6c1ca69f7fde5a4428
moody <moody@stitchfix.com>
2020-08-02 05:18:21 +0000

    Use W&B logger and expand logging

--- notebooks/abstract_model.py
+++ notebooks/abstract_model.py
@@ -1,67 +1,70 @@
 import torch
 import numpy as np
 from random import shuffle
+from torch import from_numpy
 import pytorch_lightning as pl
-
-
-def chunks(*arrays, size):
-    """Yield successive n-sized chunks from l."""
-    starts = list(range(0, len(arrays[0]), size))
-    # always visit your data in a random order!
-    shuffle(starts)
-    for i in starts:
-        arrs = [arr[i: i + size] for arr in arrays]
-        # convert numpy arrays to torch arrays
-        arrs_torch = []
-        for arr in arrs:
-            if arr.dtype != np.dtype('O'):
-                tarr = torch.from_numpy(arr)
-                arrs_torch.append(tarr)
-            else:
-                arrs_torch.append(arr)
-        yield arrs_torch[:-1], arrs_torch[-1]
+from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
+from torch.utils.data import BatchSampler
+from torch.utils.data import RandomSampler
 
 
 class AbstractModel(pl.LightningModule):
     def save_data(self, train_x, train_y, test_x, test_y, train_d=None, test_d=None):
-        self.train_x = train_x
-        self.train_y = train_y
-        self.test_x = test_x
-        self.test_y = test_y
-        self.train_d = train_d
-        self.test_d = test_d
+        if train_d is None:
+            self.train_arrs = [from_numpy(x) for x in [train_x, train_y]]
+            self.test_arrs = [from_numpy(x) for x in [test_x, test_y]]
+        else:
+            self.train_arrs = [from_numpy(x) for x in [train_x, train_y, train_d]]
+            self.test_arrs = [from_numpy(x) for x in [test_x, test_y, test_d]]
+
+    def step(self, batch, batch_nb, prefix='train', add_prior=True):
+        input, target = batch
+        prediction = self.forward(input)
+        loss = self.likelihood(prediction, target)
+        if add_prior:
+            loss = loss + self.prior()
+        tensorboard_logs = {f'{prefix}_loss': loss}
+        return {f'{prefix}_loss': loss, 'loss':loss, 'log': tensorboard_logs}
 
     def training_step(self, batch, batch_nb):
-        input, target = batch
-        prediction = self.forward(input)
-        loss = self.likelihood(prediction, target) + self.prior()
-        tensorboard_logs = {'train_loss': loss}
-        return {'loss': loss, 'log': tensorboard_logs}
-
+        return self.step(batch, batch_nb, 'train')
+    
     def test_step(self, batch, batch_nb):
-        input, target = batch
-        prediction = self.forward(input)
         # Note that we do *not* include the regularization / prior loss
         # at test time
-        loss = self.likelihood(prediction, target)
-        tensorboard_logs = {'test_loss': loss}
-        return {'test_loss': loss, 'log': tensorboard_logs}
+        return self.step(batch, batch_nb, 'test', add_prior=False)    
+    
+    def validation_step(self, batch, batch_nb):
+        return self.step(batch, batch_nb, 'val', add_prior=False)    
     
     def test_epoch_end(self, outputs):
         test_loss_mean = torch.stack([x['test_loss'] for x in outputs]).mean()
-        return {'test_loss': test_loss_mean}
+        log = {'val_loss': test_loss_mean}
+        return {'avg_test_loss': test_loss_mean, 'log': log}
+
+    def validation_epoch_end(self, outputs):
+        test_loss_mean = torch.stack([x['val_loss'] for x in outputs]).mean()
+        log = {'val_loss': test_loss_mean}
+        return {'avg_val_loss': test_loss_mean, 'log': log}
+
+    def dataloader(self, is_train=True):
+        if is_train:
+            dataset = TensorDataset(*self.train_arrs)
+        else:
+            dataset = TensorDataset(*self.test_arrs)
+        bs = BatchSampler(RandomSampler(dataset), 
+                          batch_size=self.batch_size, drop_last=False)
+        return DataLoader(dataset, batch_sampler=bs, num_workers=8)
     
     def train_dataloader(self):
-        if self.train_d is None:
-            return chunks(self.train_x, self.train_y, size=self.batch_size)
-        else:
-            return chunks(self.train_x, self.train_d, self.train_y, size=self.batch_size)
-        
+        return self.dataloader(is_train=True)
+
     def test_dataloader(self):
-        if self.test_d is None:
-            return chunks(self.test_x, self.test_y, size=self.batch_size)
-        else:
-            return chunks(self.test_x, self.test_d, self.test_y, size=self.batch_size)
+        return self.dataloader(is_train=False)
+
+    def val_dataloader(self):
+        return self.dataloader(is_train=False)
     
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-2)
+        return torch.optim.Adam(self.parameters(), lr=1e-3)
